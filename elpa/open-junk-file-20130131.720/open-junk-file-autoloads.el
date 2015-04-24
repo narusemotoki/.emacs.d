aa1da53e7b86d88c4aeac3d1b0e6b16766c5733d
Naruse Motoki <motoki@naru.se>
2015-04-24 20:54:42 +0900

    2015/04/24 20:54:42

---
+++ elpa/open-junk-file-20130131.720/open-junk-file-autoloads.el
@@ -0,0 +1,16 @@
+;;; open-junk-file-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("open-junk-file.el") (21818 11985 256221
+;;;;;;  591000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; open-junk-file-autoloads.el ends here
